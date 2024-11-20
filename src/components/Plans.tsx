a33f787567ebe4220f886b721cddeb934d9f7ce2
Angelosewase <sewasejo8@gmail.com>
2024-11-20 18:52:02 +0200

    ....

--- src/components/Plans.tsx
+++ src/components/Plans.tsx
@@ -111,7 +111,7 @@ function Plans() {
             </ul>
 
             <button className="bg-primary text-white px-6 py-3 rounded-lg hover:bg-[#14a090] transition-colors">
-              Take An Appointment
+              Try it out 
             </button>
           </div>
 
